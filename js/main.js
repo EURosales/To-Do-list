bae0e68ec08fe9e7912cff73486830609306edc1
EURosales <eduardo_rosales26@outlook.com>
2021-05-06 19:19:25 -0600

    Data fetched and displayed from firebase successfully

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-document.querySelector('.save-task').addEventListener('click', saveTask);
+document.getElementById('taskRegisterForm').addEventListener('submit', saveTask);
+const card = document.querySelector('.card-task');
+const cardHeader = document.querySelector('.card-task-header');
+const priorityLevel = document.querySelector('.priority-category');
 
 function saveTask(e) {
     e.preventDefault();
@@ -23,6 +26,7 @@ let taskRef = firebase.database().ref('Tasks');
 
 //Save the messages to the database
 function saveData(taskName, taskDate, taskDescription, priorVal) {
+    card.innerHTML = '';
     let newTaskRef = taskRef.push();
     console.log('LLegamos hasta la función de SAVEDATA()!');
     newTaskRef.set({
@@ -31,4 +35,57 @@ function saveData(taskName, taskDate, taskDescription, priorVal) {
         taskDescription: taskDescription,
         priorVal: priorVal,
     });
+}
+
+//Fetch data
+let ref = firebase.database().ref('Tasks');
+ref.on('value', getData);
+
+let priority;
+
+function getData(data) {
+    let information = data.val();
+    let keys = Object.keys(information);
+    let dateOrderedData = [];
+
+    for (let i = 0; i < keys.length; i++) {
+        let content = keys[i];
+
+        let tName = information[content].taskName;
+        let tTime = information[content].taskDate;
+        let tDescription = information[content].taskDescription;
+        let tPriority = information[content].priorVal;
+        dateOrderedData.push(information[content]);
+
+        card.innerHTML += `
+
+        <div class="card-task-header important">
+            <p class="priority-category">${tPriority}</p>
+        </div>
+        <div class="card-task-header-info">
+            <h3 class="card-task-Title">${tName}</h3>
+            <span class="card-task-date">${tTime}</span>
+        </div>
+        <div class="card-task-body-info">
+            <p class="card-description">${tDescription}</p>
+        </div>
+        <div class="card-task-footer">
+            <button onclick="func_Dat_Deletes_Card();">Mark as completed</button>
+        </div>
+
+        `;
+
+        //      info.innerHTML += `
+        //   </div>
+        //   <p class="color"><strong>Name:</strong>${name}<br>
+        //   <a><strong>Email:</strong>${email}</a><br>
+        //   <a><strong>Message:</strong>${message}</a>
+        //   </p>
+        //   </div>
+        //   `;
+    }
+    dateOrderedData.sort((a, b) =>
+        a.taskDate < b.taskDate ? -1 : a.taskDate > b.taskDate ? 1 : 0
+    );
+    console.table(dateOrderedData);
 }
